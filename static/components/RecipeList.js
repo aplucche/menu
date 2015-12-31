8a942ef0f1e62f6a0a987926928f385b52c20be9
APL <aplucche@gmail.com>
2015-12-31 04:24:57 -0500

    RecipeList fix - remove constructor

--- static/components/RecipeList.js
+++ static/components/RecipeList.js
@@ -2,10 +2,6 @@ import React, { Component, PropTypes } from 'react'
 import RecipeItem from './RecipeItem'
 
 class RecipeList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {expanded : false}
-  }
   render() {
     const { recipes } = this.props
     return (
