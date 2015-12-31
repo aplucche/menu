ec4a6d85077018a4d743f106678dcea98757ee8d
APL <aplucche@gmail.com>
2015-12-30 23:30:25 -0500

    add to directory structure

---
+++ static/components/RecipeList.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+class RecipeList extends Component {
+  render() {
+    return (
+      <div>hullo</div>
+    )
+  }
+}
+
+export default RecipeList
---
+++ static/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react'
+import RecipeList from '../components/RecipeList'
+
+class App extends Component {
+  render() {
+    return (
+      <RecipeList />       
+    )
+  }
+}
+
+export default App
