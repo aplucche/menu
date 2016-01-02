53547613163a486ff04048841895e3a6b19917c7
APL <aplucche@gmail.com>
2016-01-02 00:39:19 -0500

    move test data from App.js to seperate file

--- static/containers/App.js
+++ static/containers/App.js
@@ -1,73 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 import RecipeList from '../components/RecipeList'
 import RecipeForm from '../components/RecipeForm'
+import fillerData from '../dev/FillerData'
 
 class App extends Component {
  constructor(props) {
     super(props);
-    this.state = {recipes : [
-              {
-                "id" : 1,
-                "name" :"Sausage Herb Stuffing",
-                "description": "",
-                "link" :"http://www.foodnetwork.com/recipes/ina-garten/sausage-and-herb-stuffing-recipe.html",
-                "category" :"side",
-                "notes" :"",
-                "isSelected": false,
-                "isEditing": false
-              },
-              {
-                "id" : 2,
-                "name" :"Butternut Squash Soup",
-                "description": "",
-                "link" :"http://www.foodnetwork.com/recipes/food-network-kitchens/butternut-squash-soup-recipe1.html?vty=recipes/butternut-squash-soup-recipe.html",
-                "category" :"appetizer",
-                "notes" :"###Tips at###  http://cookieandkate.com/2015/roasted-butternut-squash-soup/",
-                "isSelected": false,
-                "isEditing": false
-              },
-              {
-                "id" : 3,
-                "name" :"Fitgerald",
-                "description": "Hendrix gin with lemon simple syrup and bitters",
-                "link" :"http://www.thekitchn.com/forgotten-gin-cocktails-part-1-166935",
-                "category" :"drink",
-                "notes" :"",
-                "isSelected": false,
-                "isEditing": false
-              },
-              {
-                "id" : 4,
-                "name" :"Baked Apples",
-                "description" :"filled with cinnamon and oats",
-                "link" :"http://www.thekitchn.com/recipe-baked-apples-stuffed-with-oatmeal-brown-sugar-75752",
-                "category" :"dessert",
-                "notes" :"",
-                "isSelected": false,
-                "isEditing": false
-              },
-              {
-                "id" : 5,
-                "name" :"Cranberry Sauce",
-                "description": "",
-                "link" :"http://www.foodnetwork.com/recipes/food-network-kitchens/perfect-cranberry-sauce-recipe.html",
-                "category" :"side",
-                "notes" :"",
-                "isSelected": false,
-                "isEditing": true
-              },
-              {
-                "id" : 6,
-                "name" :"Mojito",
-                "description": "Muddled mint and simple syrup with rum and club soda",
-                "link" :"http://www.foodnetwork.com/recipes/food-network-kitchens/perfect-cranberry-sauce-recipe.html",
-                "category" :"drink",
-                "notes" :"",
-                "isSelected": false,
-                "isEditing": false
-              }
-            ]
-    }
+    this.state = fillerData
   }
   editItem(id) {
   var editedRecipes = this.state.recipes.map(recipe => 
