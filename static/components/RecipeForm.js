8ff8e206532bb73dc18cf345d95b8e919ae0fb6c
APL <aplucche@gmail.com>
2016-01-02 02:44:14 -0500

    add styling

--- static/components/RecipeForm.js
+++ static/components/RecipeForm.js
@@ -1,36 +1,30 @@
 import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
 
 class RecipeForm extends Component {
   render() {
     const { recipe, formCancelClick, formSaveClick } = this.props
     return (
       <div className='formContainer'>
-        <label forHtml='recipeName'>Name:</label>
-          <br/>
-          <input name='recipeName' ref='recipeName' type='text' defaultValue={ recipe.name }/>
-          <br/>
-        <label forHtml='recipeDescription'>Description:</label>
-          <br/>
-          <input name='recipeDescription' ref='recipeDescription' type='text' defaultValue={ recipe.description }/>
-          <br/>
-        <label forHtml='recipeLink'>Link:</label>
-          <br/>
-          <input name='recipeLink' ref='recipeLink' type='text' defaultValue={ recipe.link }/>
-          <br/>
-        <label forHtml='recipeNotes'>Notes:</label>
-          <br/>
-          <textarea name='recipeNotes' ref='recipeNotes' rows='3' defaultValue={ recipe.notes }/>
         <div className='formButtonRow'>
           <button className='formSave' onClick={() => formSaveClick(recipe.id, 
               {
-                'name': React.findDOMNode(this.refs.recipeName).value,
-                'description': React.findDOMNode(this.refs.recipeDescription).value,
-                'link': React.findDOMNode(this.refs.recipeLink).value,
-                'notes': React.findDOMNode(this.refs.recipeNotes).value
+                'name': ReactDOM.findDOMNode(this.refs.recipeName).value,
+                'description': ReactDOM.findDOMNode(this.refs.recipeDescription).value,
+                'link': ReactDOM.findDOMNode(this.refs.recipeLink).value,
+                'notes': ReactDOM.findDOMNode(this.refs.recipeNotes).value
               }  
             )}>save</button>
           <button className='formCancel' onClick={() => formCancelClick(recipe.id) }>cancel</button>
         </div>
+        <label forHtml='recipeName'>Name:</label>
+          <input name='recipeName' ref='recipeName' type='text' defaultValue={ recipe.name }/>
+        <label forHtml='recipeDescription'>Description:</label>
+          <input name='recipeDescription' ref='recipeDescription' type='text' defaultValue={ recipe.description }/>
+        <label forHtml='recipeLink'>Link:</label>
+          <input name='recipeLink' ref='recipeLink' type='text' defaultValue={ recipe.link }/>
+        <label forHtml='recipeNotes'>Notes:</label>
+          <textarea name='recipeNotes' ref='recipeNotes' rows='3' defaultValue={ recipe.notes }/>
       </div>
       )
   }
