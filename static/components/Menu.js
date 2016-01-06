65d90efadce06381ca38bae1c95f3f5477a229c5
APL <aplucche@gmail.com>
2016-01-05 20:15:28 -0500

    add additional menu data

--- static/components/Menu.js
+++ static/components/Menu.js
@@ -5,6 +5,11 @@ class Menu extends Component {
     const { recipes, menuStyle } = this.props
     return (
       <div className='menuContainer' style={menuStyle.menuContainer} >
+        <div className='menuHeader' style={menuStyle.menuHeaderStyle} >
+          <div className='menuTitle'>{menuStyle.menuHeader.title}</div>
+          <div className='menuDescription'>{menuStyle.menuHeader.description}</div>
+          <div className='menuDate'>{menuStyle.menuHeader.date}</div>
+        </div>
         {recipes.map(recipe => (recipe.isSelected === true) ? 
         <div className='menuItem' style={menuStyle.menuItem} key={recipe.id}>
           <div className='menuItemName' style={menuStyle.menuItemName} >{recipe.name}</div>
