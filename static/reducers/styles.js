eb5f8a3ac46b14c12f24069ec589ecac40b0531f
APL <aplucche@gmail.com>
2016-01-22 16:37:47 -0500

    new style template

--- static/reducers/styles.js
+++ static/reducers/styles.js
@@ -146,7 +146,53 @@ const initialState = {
           "fontStyle": "italic",
           "textAlign": "center"
         }
-      },  
+      },
+      old: {
+        menuContainer: {
+          fontFamily: "Libre Baskerville",
+          background: "#3E4147",
+          color: "#C02942",
+          display: "flex",
+          flexFlow: "column nowrap",
+          alignItems: "center",
+          width: 500,
+          border: "1px solid #2A2C31"
+        },
+        menuHeaderStyle : {
+          display: "flex",
+          flexFlow: "column nowrap",
+          color: "#C02942",
+          fontSize: "155%",
+          alignItems: "center",
+          fontWeight: "bold",
+          marginTop: "15px"
+        },
+        menuCategoryContainer: {
+          display: "flex",
+          fontSize: "1l5%",
+          flexFlow: "column nowrap",
+          alignItems: "center",
+          marginTop: "10px",
+          borderBottom: "solid 1px #2A2C31"
+        },
+        menuCategory: {
+          fontWeight: "bold",
+          color: "#DFBA69"
+        },
+        menuItem: {
+          display: "flex",
+          flexFlow: "column nowrap",
+          alignItems: "center",
+          "textAlign": "center",
+          marginTop: "10px",
+          paddingBottom: "10px"
+        },
+        menuItemName: {color: "#FFFEDF"},
+        menuItemDescription: {
+          "fontStyle": "italic",
+          "textAlign": "center"
+        }
+      },
       minimal: {
         menuHeaderStyle : {},
         menuContainer: {color:'blue'},
